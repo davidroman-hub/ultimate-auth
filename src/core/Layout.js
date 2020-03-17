aeb5c72a4f7a94649e75ae004f29884c2b5e62e3
davidroman-hub <jobroman83@gmail.com>
2020-03-17 18:59:15 +0100

    active link path

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -3,17 +3,30 @@ import {Link, withRouter} from 'react-router-dom'
 
 
 
-const Layout = ({children}) => {
+const Layout = ({children,match}) => {
+
+const isActive = path => {
+    if(match.path === path) {
+        return {color:'#000'}
+    } else{
+        return {color:'#fff'}
+    }
+};
+
+
     const nav = () => (
         <ul className="nav nav-tabs bg-primary">
                 <li className="nav-item">
-                    <Link to='/' className="text-light nav-link"> home </Link>
+                    <Link to='/' className=" nav-link" style={isActive('/')}>
+                         {/* home / {JSON.stringify(history)}     */}
+                         home
+                    </Link>
                 </li>
                 <li className="nav-item">
-                    <Link to='/signup' className="text-light nav-link"> SignUp </Link>
+                    <Link to='/signup' className=" nav-link" style={isActive('/signup')}> SignUp </Link>
                 </li>
                 <li className="nav-item">
-                    <Link to='/signin' className="text-light nav-link"> SignIn </Link>
+                    <Link to='/signin' className=" nav-link" style={isActive('/signin')}> SignIn </Link>
                 </li>
         </ul>
     )
